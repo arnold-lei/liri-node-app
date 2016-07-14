d274a75c3486055d701f0f54cf54a9fb42e13f93
Arnold Lei <arnold.lei88@gmail.com>
2016-07-14 19:40:45 -0400

    testing different methods

--- term_test.js
+++ term_test.js
@@ -17,14 +17,8 @@ var _TWITTER_CONSUMER_SECRET = keys.twitterKeys.consumer_secret;
 var _TWITTER_ACCESS_TOKEN_KEY = keys.twitterKeys.access_token_key;
 var _TWITTER_ACCESS_TOKEN_SECRET = keys.twitterKeys.access_token_secret;
 
-var items = ['My Tweets', 'Spotify Song', 'Look Up Movie'];
-var options = {
-	style: term.inverse ,
-	selectedStyle: term.white.bgYellow
-};
-var welcomeMsg = 'Hi I am LIRI. I can give you song information, movie information, or your latest tweets, what can I do for you?'  + '\r\n\r\n';
+var latestTweets; 
 
-//****** SETTING UP TWITTER ******** 
 var lastTweets = function(){
 	var tweetClient = new Twitter({
 		consumer_key: _TWITTER_CONSUMER_KEY,
@@ -39,46 +33,31 @@ var lastTweets = function(){
 	tweetClient.get('statuses/user_timeline', params, function(error, tweets, response){
 		if (!error){
 			for (var i = 0; i < 10; i++){
-				 lastestTweets =  tweets[i].text;
-				  term(tweets[i].text); 
+				 lastestTweets.push(tweets[i].text);
+				 console.log(tweets[i].text)
 			}
 			
 		}else{
 			term(error)
 		}
+		
 	})
 	// process.exit();
 }
-term.clear();
 
-
-
-var intro = function(){
+var printTweets = function(){
 	term.slowTyping(
-		welcomeMsg ,
-		{ 
-			flashStyle: term.yellow,
+	  	latestTweets.toString() ,
+	  	{ 
+	  		flashStyle: term.yellow,
 			delay: 10, 
-		},
-		function() {
-			term.singleLineMenu( items , options , function( error , response) {
-				term( '\n' ).eraseLineAfter.green(
-					"#%s selected: %s (%s,%s)\n" ,
-					response.selectedText,
-					response.x,
-					response.y,
-					term(lastTweets()),
-					// response(lastTweets())
-					
-					setTimeout(function(){ process,exit(); }, 3000) 
-				);
-			
-			} );
-		}
-	) ;
+	  	},
+	 	function(){ process.exit(); }
+	 ); 
 
 }
 
-intro()
 
+term.clear();
 
+printTweets();
